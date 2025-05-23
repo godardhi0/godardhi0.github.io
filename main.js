a9f522a0bb3e9aa7d34efbba9c24d0cc5710cbf1
godardhi0 <ishimwegodardquanta66@gmail.com>
2025-05-23 21:17:46 +0200

    changes done in main.js

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 function displayDateTime()
 {
-    const now = new Date();
+    const last_modified = new Date('2025-05-23T21:16:00');
     const options = { 
+        day: '2-digit', 
+        month: '2-digit', 
         year: 'numeric', 
-        month: 'long', 
-        day: 'numeric', 
         hour: '2-digit', 
         minute: '2-digit', 
         second: '2-digit', 
         hour12: false 
     };
-            const formattedDate = now.toLocaleString('FR', options); // Format the date and time
+            const formattedDate = now.toLocaleString('fr-FR', options); // Format the date and time
             document.getElementById('dateTime').innerText = formattedDate; // Display it in the paragraph
 }
 
